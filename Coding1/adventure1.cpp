9b05b79360350ca1b6709df14343c9ad6234e0a5
Brian Foster <brianisbrilliant@users.noreply.github.com>
2025-02-11 10:05:23 -0600

    finished first C++ homework

--- Coding1/adventure1.cpp
+++ Coding1/adventure1.cpp
@@ -1,43 +1,67 @@
 
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
-	cout << "asdf\n";
     //welcome the player
-    cout << "Hello player! Ready for adventure?\n";
-        //set up the game
-    int health = 10; 
-    int attack,
-    int block; 
+    //set up the game
+    int health = 10;
+    int attack = 0;
+    int block = 0;
     int turns = 0;
-        //seed the random number generator
-        //start the loop
-        //add 1 to turns
-        //start the encounter
-        //randomly generate numbers for attack(range = 0 - 4) and block(range = 0 - 4)
-        //if block is greater or equal to attack, successful block
-            //otherwise, subtract attack value from health.
-            //ask the player if they would like to keep adventuring. if they don't, break out of the loop.
-            //keep looping while health is greater than zero and fewer than 4 turns have happened
+    //seed the random number generator
+    srand(time(0));
+    //start the loop
+    do {
+        
+        turns += 1;                                                 //add 1 to turns
+        
+        cout << "An enemy attacks!\n";                              //start the encounter
+  
+        attack = rand() % 5;                                        //randomly generate numbers for attack(range = 0 - 4) and block(range = 0 - 4)
+        block = rand() % 5;
+        
+        if (block >= attack) {                                      //if block is greater or equal to attack, successful block
+            cout << "Successful block!\n";
+        }
+        else {                                                      //otherwise, subtract attack value from health.
+            cout << "You've been hit!\n";
+            health -= attack;
+            cout << "Your health is now " << health << ".\n";
+        }
 
-            //if health is greater than 0, congratulate player
-                //otherwise, tell the player they're dead.
-
-    while (true) {
-        cout << "Keep playing?\n";
+        if (health < 1) {
+            cout << "You have died!\n";
+            break;
+        }
+        
+        cout << "Would you like to keep adventuring?\nType 'yes' to continue: ";            //ask the player if they would like to keep adventuring. if they don't, break out of the loop.
         string input;
+
         cin >> input;
 
-        if (input == "yes") {
-            cout << "Here we go again!\n";
+        if (input != "yes") {
+            break;
+            cout << "Going home now!\n";
         }
-        else {
-            cout << "Thanks for playing!\n";
-            break;      // this breaks us out of the loop early!
-        }
+
+    } while (health > 0 && turns < 8);                             //keep looping while health is greater than zero and fewer than 4 turns have happened
+
+
+        //if health is greater than 0, congratulate player
+    if (health > 0) {
+        cout << "You survived your adventure!\n";
     }
+    //otherwise, tell the player they're dead.
+    else {
+        cout << "Your body is laying by the road. Enjoy the afterlife, if it exists.\n";
+    }
+            
+
 
 
 
